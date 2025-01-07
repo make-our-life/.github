b34e76bb28ddae10d41e65cf365ff191c3406162
gajun.choi <5tmdhkfem@gmail.com>
2025-01-07 22:01:16 +0900

    feat: input box 생성 - forwardRef 를 이용한 커서 이벤트 컨트롤 - error 시 input box css 변경 처리 (className 변경로직)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css'
 import BoardItem from './components/BoardItem';
 import top3BoardListMock from './mocks/top3-board-list.mock';
 import Top3Item from './components/Top3Item';
@@ -6,9 +7,12 @@ import CommentItem from './components/CommentItem';
 import commentListMock from './mocks/comment-list.mock';
 import favoriteListMock from './mocks/favorite-list.mock';
 import FavoriteItem from './components/FavoriteItem';
+import InputBox from './components/InputBox';
 
 
 function App() {
+
+  const [value, setValue] = useState<string>('');
 
   return (
     <>
@@ -16,9 +20,11 @@ function App() {
       <div style={{padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px'}}>
         {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem}/>)}
       </div> */}
+      {/* 좋아요 리스트트
       <div style={{display: 'flex', columnGap: '30px', rowGap: '20px'}}>
         {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem}/>)}
-      </div>
+      </div> */}
+      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaaa'/>
     </>
   )
 }
---
+++ src/components/InputBox/index.tsx
@@ -0,0 +1,61 @@
+import {ChangeEvent, Dispatch, forwardRef, SetStateAction, KeyboardEvent} from 'react'
+import './style.css'
+
+// interface: Input Box 컴포넌트 Properties
+interface Props {
+    // input 버튼은 type 구분 해야함
+    label: string;
+    type: 'text' | 'password';
+    placeholder: string;
+    value: string;
+    setValue: Dispatch<SetStateAction<string>>; // state의 타입입
+    error: boolean; // error 상태 구분
+
+    icon?: string; // 값이 없을 수도 있음
+    onButtonClick?: () => void;
+
+    message?: string;
+
+    onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+// component: Input Box 컴포넌트 (forwardRef 로 input 버튼 컨트롤 : ref -> 엔터 클릭 시 input 커서서 넘기기)
+const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+    
+    // state: properties
+    const { label, type, placeholder, value, error, icon, message } = props;
+    const { setValue, onButtonClick, onKeyDown } = props;
+
+    // event handler : input 값 변경 이벤트 처리 함수
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setValue(value);
+    }
+
+    // event handler : input 키 이벤트 처리 함수
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if(!onKeyDown) return;
+        onKeyDown(event);
+    }
+
+    // render: Input Box 컴포넌트
+    return (
+        <div className='inputbox'>
+            <div className='inputbox-label'>{label}</div>
+            <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+                <input className='input' ref={ref} type={type} placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownHandler}/>
+                {onButtonClick !== undefined && (
+                    <div className='icon-button'>
+                        {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
+                    </div>
+                )}
+                
+            </div>
+            {message !== undefined && (
+                <div className='inputbox-message'>{message}</div>
+            )}
+        </div>
+    )
+})
+
+export default InputBox;
