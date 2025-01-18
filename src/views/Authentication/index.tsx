8547ea31345437302f6c860eb3743235115266e6
gajun.choi <5tmdhkfem@gmail.com>
2025-01-18 22:32:21 +0900

    feat: auth 화면 구축(1) container css작업

--- src/views/Authentication/index.tsx
+++ src/views/Authentication/index.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 // component: 인증 화면 컴포넌트
 export default function Authentication() {
+
+  // state: 화면 상태 (view : sign-in / sign-up 두 가지 상태만 가질 수 있음음)
+  const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
+  // component: sign-in card 컴포넌트 
+  const SignInCard = () => {
+    return (
+      <div className='auth-card'></div>
+    )
+  }
+
+  // component: sign-up card 컴포넌트 
+  const SignUpCard = () => {
+    return (
+      <div className='auth-card'></div>
+    )
+  }
+
     // render: 인증 화면 컴포넌트 랜더링링
   return (
-    <div>Authentication</div>
+    <div id='auth-wrapper'>
+      <div className='auth-container'>
+        <div className='auth-jumbotron-box'>
+          <div className='auth-jumbotron-contents'>
+            <div className='auth-logo-icon'></div>
+            <div className='auth-jumbotron-text-box'>
+              <div className='auth-jumbotron-text'>{'환영한다'}</div>
+              <div className='auth-jumbotron-text'>{'게이들아'}</div>
+            </div>
+          </div>
+        </div>
+          {view === 'sign-in' && <SignInCard/>}
+          {view === 'sign-up' && <SignUpCard/>}
+      </div>
+    </div>
   )
 }
